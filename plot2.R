8894cfacd02fb981fc5d89865edfed34e3ddf609
Christine Rehwinkel <christinerehwinkel@verizon.net>
2015-08-07 16:31:02 -0400

    Programming assignment 1

---
+++ plot2.R
@@ -0,0 +1,22 @@
+#Plot 1 - Programming Assignment 1
+setwd("C:/Users/crehwinkel/My Documents/DataScience/ExploreData/ProgramAssign1")
+
+newFile <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", nrows= 1000000, 
+                      stringsAsFactors=FALSE)
+
+library(dplyr)
+newFile$DateTime <- paste(newFile$Date, " ", newFile$Time)
+newFile$DateTime <- as.Date(newFile$DateTime, format = "%d/%m/%Y %H:%M:%S")
+
+subs <- filter(newFile, DateTime >= as.Date("2007-02-01 00:00:00"), DateTime < as.Date("2007-02-03 00:00:00"))
+
+subs1 <- mutate(subs, CombDT = paste(subs$Date, " ", subs$Time))
+subs1$CombDT <- as.POSIXlt(subs1$CombDT, format = "%d/%m/%Y %H:%M:%S")
+
+
+plot(subs1$CombDT , subs1$Global_active_power, ylim = range(0:8),
+     ylab = "Global Active Power (kilowatts)", xlab = "", type = "l")
+
+
+dev.copy(png, file = "plot2.png")
+dev.off()
